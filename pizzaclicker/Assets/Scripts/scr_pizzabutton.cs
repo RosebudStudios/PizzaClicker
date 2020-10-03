57241be96a4afe865ea4993b39f92dda22cb1540
Connor Voorhees <cvbor999@gmail.com>
2020-10-03 13:45:29 -0400

    Clicker Mechanic Implemented

---
+++ pizzaclicker/Assets/Scripts/scr_pizzabutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_pizzabutton : MonoBehaviour
+{
+    public GameObject gc;
+
+    public void addslice ()
+    {
+        gc.GetComponent<scr_gamecontroller>().slices++;
+    }
+
+}
---
+++ pizzaclicker/Assets/gamecontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_gamecontroller : MonoBehaviour
+{
+    public int pizzas;
+    public int slices;
+
+    public Text slicetext;
+    public Text pizzatext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slices = 0;
+        pizzas = 0;
+        slicetext.text = "";
+        pizzatext.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slicetext.text = "Slices: " + slices;
+        pizzatext.text = "Pizzas: " + pizzas;
+        if (slices >= 8) { pizzas++; slices = 0; }
+    }
+}
